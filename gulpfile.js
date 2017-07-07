5c5cd592f187157e80e38856e9bf657346b89697
Neil Young <neil.young2010@gmail.com>
2017-07-07 10:58:56 +0000

    Adding php starter package

---
+++ gulpfile.js
@@ -0,0 +1,207 @@
+const gulp = require('gulp');
+const gulpLoadPlugins = require('gulp-load-plugins');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+const runSequence = require('run-sequence');
+const connect = require('gulp-connect-php');
+const httpProxy = require('http-proxy');
+
+const source = require('vinyl-source-stream');
+const browserify = require('browserify');
+const babelify = require('babelify');
+//const wiredep = require('wiredep').stream;
+
+const $ = gulpLoadPlugins();
+const reload = browserSync.reload;
+
+let dev = true;
+let port = 9000;
+
+gulp.task('styles', () => {
+    return gulp.src('src/css/*.scss')
+        .pipe($.plumber())
+        .pipe($.if(dev, $.sourcemaps.init()))
+        .pipe($.sass.sync({
+            outputStyle: 'expanded',
+            precision: 10,
+            includePaths: ['.']
+        }).on('error', $.sass.logError))
+        .pipe($.autoprefixer({
+            browsers: ['> 1%', 'last 2 versions', 'Firefox ESR']
+        }))
+        .pipe($.if(dev, $.sourcemaps.write()))
+        .pipe(gulp.dest('.tmp/css'))
+        .pipe(reload({
+            stream: true
+        }));
+})
+
+gulp.task('scripts', () => {
+
+    return browserify("src/js/index.js", {
+            debug: true
+        })
+        .transform(babelify)
+        .bundle()
+        .pipe(source('index.js'))
+        .pipe(gulp.dest('.tmp/js'))
+        .pipe(reload({
+            stream: true
+        }))
+});
+
+function lint(files) {
+    return gulp.src(files)
+        .pipe($.eslint({
+            fix: true
+        }))
+        .pipe(reload({
+            stream: true,
+            once: true
+        }))
+        .pipe($.eslint.format())
+        .pipe($.if(!browserSync.active, $.eslint.failAfterError()));
+}
+
+gulp.task('lint', () => {
+    return lint('scr/scripts/**/*.js')
+        .pipe(gulp.dest('src/scripts'));
+})
+
+gulp.task('html', ['styles', 'scripts'], () => {
+    return gulp.src(['src/*.html', 'src/*.php'])
+        .pipe($.useref({
+            searchPath: ['.tmp', 'src', '.']
+        }))
+        .pipe($.if(/\.js$/, $.uglify({
+            compress: {
+                drop_console: true
+            }
+        })))
+        .pipe($.if(/\.css$/, $.cssnano({
+            safe: true,
+            autoprefixer: false
+        })))
+        .pipe(gulp.dest('dist'));
+})
+
+// gulp.task('bower', () => {
+//   gulp.src('src/*.html')
+//   .pipe(wiredep())
+//   .pipe(gulp.dest('src'));
+// })
+
+
+gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
+
+gulp.task('php-serve', () => {
+    connect.server({
+        port: 9001,
+        base: 'src',
+        open: false
+    });
+
+    var proxy = httpProxy.createProxyServer({});
+
+    runSequence(['clean'], ['styles', 'scripts'], () => {
+      browserSync.init({
+          notify: false,
+          port  : 9000,
+          server: {
+              baseDir   : ['.tmp', 'src'],
+              routes    : {
+                  '/bower_components': 'bower_components'
+              },
+              middleware: function (req, res, next) {
+                  var url = req.url;
+
+                  if (!url.match(/^\/(css|fonts|bower_components)\//)) {
+                      proxy.web(req, res, { target: 'http://127.0.0.1:9001' });
+                  } else {
+                      next();
+                  }
+              }
+          }
+      });
+    });
+
+    // watch for changes
+    gulp.watch([
+        'src/*.html',
+        'src/js/**/*.js',
+        'src/*.php',
+    ]).on('change', reload);
+
+    gulp.watch('src/css/**/*.scss', ['styles']);
+    gulp.watch('src/js/**/*.js', ['scripts']);
+});
+
+gulp.task('serve', () => {
+    runSequence(['clean'], ['styles', 'scripts'], () => {
+        browserSync.init({
+            notify: false,
+            port: port,
+            server: {
+                baseDir: ['.tmp', 'src'],
+                ui: false,
+                routes: {
+                    '/bower_components': 'bower_components'
+                }
+            }
+        });
+    });
+
+    gulp.watch([
+        'src/*.html',
+        'src/js/**/*.js'
+    ]).on('change', reload);
+
+    gulp.watch('src/css/**/*.scss', ['styles']);
+    gulp.watch('src/js/**/*.js', ['scripts']);
+
+})
+
+gulp.task('serve:dist', ['default'], () => {
+    browserSync.init({
+        notify: false,
+        port: port,
+        server: {
+            baseDir: ['dist']
+        }
+    });
+});
+
+gulp.task('php-serve:dist', ['default'], () => {
+  connect.server({
+      port: 9001,
+      base: 'dist',
+      open: false
+  });
+
+  var proxy = httpProxy.createProxyServer({});
+
+  browserSync.init({
+      notify: false,
+      port  : 9000,
+      server: {
+          baseDir   : ['dist'],
+          middleware: function (req, res, next) {
+              var url = req.url;
+
+              if (!url.match(/^\/(css|js)\//)) {
+                  proxy.web(req, res, { target: 'http://127.0.0.1:9001' });
+              } else {
+                  next();
+              }
+          }
+      }
+  });
+});
+
+gulp.task('build', ['lint', 'html']);
+gulp.task('default', () => {
+    return new Promise(resolve => {
+        dev = false;
+        runSequence(['clean'], 'build', resolve);
+    });
+})
---
+++ src/index.php
@@ -0,0 +1,37 @@
+<?php
+$hello = "Hello World!";
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta content="yes" name="apple-mobile-web-app-capable" />
+    <meta content="yes" name="apple-touch-fullscreen" />
+    <meta content="telephone=no,email=no" name="format-detection" />
+    <title>market knowledge capital</title>
+    <!-- Place favicon.ico in the root directory -->
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <!--lib-flexible (https://github.com/amfe/lib-flexible)-->
+    <script>!function(a,b){function c(){var b=f.getBoundingClientRect().width;b/i>540&&(b=540*i);var c=b/10;f.style.fontSize=c+"px",k.rem=a.rem=c}var d,e=a.document,f=e.documentElement,g=e.querySelector('meta[name="viewport"]'),h=e.querySelector('meta[name="flexible"]'),i=0,j=0,k=b.flexible||(b.flexible={});if(g){console.warn("将根据已有的meta标签来设置缩放比例");var l=g.getAttribute("content").match(/initial\-scale=([\d\.]+)/);l&&(j=parseFloat(l[1]),i=parseInt(1/j))}else if(h){var m=h.getAttribute("content");if(m){var n=m.match(/initial\-dpr=([\d\.]+)/),o=m.match(/maximum\-dpr=([\d\.]+)/);n&&(i=parseFloat(n[1]),j=parseFloat((1/i).toFixed(2))),o&&(i=parseFloat(o[1]),j=parseFloat((1/i).toFixed(2)))}}if(!i&&!j){var p=(a.navigator.appVersion.match(/android/gi),a.navigator.appVersion.match(/iphone/gi)),q=a.devicePixelRatio;i=p?q>=3&&(!i||i>=3)?3:q>=2&&(!i||i>=2)?2:1:1,j=1/i}if(f.setAttribute("data-dpr",i),!g)if(g=e.createElement("meta"),g.setAttribute("name","viewport"),g.setAttribute("content","initial-scale="+j+", maximum-scale="+j+", minimum-scale="+j+", user-scalable=no"),f.firstElementChild)f.firstElementChild.appendChild(g);else{var r=e.createElement("div");r.appendChild(g),e.write(r.innerHTML)}a.addEventListener("resize",function(){clearTimeout(d),d=setTimeout(c,300)},!1),a.addEventListener("pageshow",function(a){a.persisted&&(clearTimeout(d),d=setTimeout(c,300))},!1),"complete"===e.readyState?e.body.style.fontSize=12*i+"px":e.addEventListener("DOMContentLoaded",function(){e.body.style.fontSize=12*i+"px"},!1),c(),k.dpr=a.dpr=i,k.refreshRem=c,k.rem2px=function(a){var b=parseFloat(a)*this.rem;return"string"==typeof a&&a.match(/rem$/)&&(b+="px"),b},k.px2rem=function(a){var b=parseFloat(a)/this.rem;return"string"==typeof a&&a.match(/px$/)&&(b+="rem"),b}}(window,window.lib||(window.lib={}));</script>
+    <!-- lib-flexible end-->
+
+    <!-- build:css css/index.css -->
+    <link rel="stylesheet" href="../bower_components/normalize-css/normalize.css" />
+    <link rel="stylesheet" href="css/index.css">
+    <!-- endbuild -->
+  </head>
+  <body>
+    <div class="container">
+      <p>
+        <?php echo $hello; ?>
+      </p>
+    </div>
+    <!-- build:js js/index.js -->
+    <script src="../bower_components/jquery/dist/jquery.js"></script>
+    <script src="js/index.js"></script>
+    <!-- endbuild -->
+  </body>
+</html>
